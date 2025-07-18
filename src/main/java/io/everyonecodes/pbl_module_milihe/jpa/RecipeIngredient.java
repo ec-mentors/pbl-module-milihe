b38545b061aec930bbfc31c2ae004eab7ee3d687
Milla Herth <kuruczmilla@gmail.com>
2025-07-18 09:37:45 +0200

    modified gitignore

--- src/main/java/io/everyonecodes/pbl_module_milihe/jpa/RecipeIngredient.java
+++ src/main/java/io/everyonecodes/pbl_module_milihe/jpa/RecipeIngredient.java
@@ -1,17 +1,4 @@
 package io.everyonecodes.pbl_module_milihe.jpa;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class RecipeIngredient {
-    private int recipeId;
-    private int ingredientId;
-    private double amount;
-    private String unit;
 }
